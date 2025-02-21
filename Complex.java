ca5dca3c1951c8c9ba5ab5c272dc8fe89e8b7721
VYSHNAVISVARIYAR <vyshnavisvariyar2002@gmail.com>
2025-02-21 16:24:03 +0530

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public static void main(String[] args){
+    System.out.println("Enter the real part of the first complex number:");
+    Scanner sc = new Scanner(System.in);
+    int r1 = sc.nextInt();
+
+    System.out.println("Enter the imaginery part of the first complex number:");
+    int r2 = sc.nextInt();
+
+
+}
