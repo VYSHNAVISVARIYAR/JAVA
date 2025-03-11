44d520ef05505de6e391aae8f4cc25abf6c4a97c
VYSHNAVISVARIYAR <vyshnavisvariyar2002@gmail.com>
2025-03-11 12:20:15 +0530

    Add files via upload

---
+++ StringManupulation.java
@@ -0,0 +1,74 @@
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String string = "vyshnavi";
+		char character = string.charAt(5);
+		System.out.println(character); 
+	}
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String one = "one";
+		String two = "one";
+		System.out.println(one.compareTo(two));//returns 0 because both the words are equal
+	}
+
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String name1 = "Vyshnavi";
+		String name2 = "Variyar";
+		
+		System.out.println(name1.concat(" "+name2));
+	}
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String name = "vyshnavi";
+		System.out.println(name.endsWith("vi"));
+		System.out.println(name.endsWith("o"));
+		}
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String name1 = "vyshnavi";
+		String name2 = "variyar";
+		System.out.println(name1.equals(name2));
+		}
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String string = "vyshnavi";
+		System.out.println(string.toUpperCase());
+		System.out.println(string.toLowerCase());
+		}
+}
+*/
+
+/*public class StringManupulation{
+	public static void main(String[] args)
+	{
+		String string = "vyshnavi     ";
+		System.out.println(string);
+		System.out.println(string.trim());
+		}
+}
+*/
+
+
+
