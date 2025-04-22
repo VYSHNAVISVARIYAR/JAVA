c8a992f063121e8276097688d9aee22e80134a22
VYSHNAVISVARIYAR <vyshnavisvariyar2002@gmail.com>
2025-04-22 15:50:21 +0530

    Add files via upload

---
+++ ExceptionExample.java
@@ -0,0 +1,21 @@
+public class ExceptionExample {
+    public static void main(String[] args) {
+	try{
+        int result = 10 / 0; 
+        System.out.println("Result: " + result);
+       
+	}catch(ArithmeticException ae)
+		{
+		System.out.println("Divide by zero error at line no 4"+ae);
+		}
+    finally{
+        System.out.println("100");
+        for(int i=0;i<10;i++)
+        {
+            System.out.print(i+" ");
+        }
+    
+    }
+        
+    }
+}
